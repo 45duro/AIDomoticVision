e94e2d2ea71c3692d7f269aba96e2fa0c2d5dd5d
45duro <jonathangc.awt@gmail.com>
2019-06-05 11:56:09 -0500

    Aprendiendo a manejar el open CV

---
+++ DominandoImagenes.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+
+def mostrarImagen(imagen):
+    cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('image', imagen)
+    cv2.waitKey()
+
+
+
+
+filename = "rostro.jpeg"
+
+#Leer la imagen
+img = cv2.imread(filename)
+
+#leer en grises
+#img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+
+#Convertir a grises
+grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# find minimum and maximum intensities
+# Convertir  en negros y blancos sobelx
+sobelx = cv2.Sobel(grey, cv2.CV_32F, 1, 0)
+minVal = np.amin(sobelx)
+maxVal = np.amax(sobelx)
+draw = cv2.convertScaleAbs (sobelx, alpha = 255.0 / (maxVal - minVal), beta = -minVal * 255.0 / (maxVal - minVal))
+
+
+
+#mi funcion para mostrar la imagen
+mostrarImagen(draw)
